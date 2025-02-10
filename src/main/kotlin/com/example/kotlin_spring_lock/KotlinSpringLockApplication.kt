b1867a66453afe65fbec0a77cfc199fe2ad44480
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-02-10 13:12:35 +0900

    first

---
+++ src/main/kotlin/com/example/kotlin_spring_lock/KotlinSpringLockApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_lock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringLockApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringLockApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_spring_lock/KotlinSpringLockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_spring_lock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringLockApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
