37c245be35180fe6dbfa352714c97150577a030c
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-02-10 19:08:29 +0900

    refactor: 코드 리팩터링

--- src/main/kotlin/com/example/kotlin_spring_lock/service/CouponService.kt
+++ src/main/kotlin/com/example/kotlin_spring_lock/service/CouponService.kt
@@ -14,47 +14,40 @@ class CouponService(
     private val memberRepository: MemberRepository
 ) {
 
+    // 널처리에 대한 고민 ?, !!
     @Transactional
     fun issueCoupon(memberId: Long): Coupon {
+        // 비관 락
         couponRepository.lockCouponsTable()
 
         // 쿠폰 갯수가 100장 이상이라면 404 반환
-        val couponCount = couponRepository.count()
-        if (couponCount >= 100) {
-            throw ResponseStatusException(HttpStatus.NOT_FOUND, "쿠폰이 끝났습니다")
+        if (couponRepository.count() >= 100) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "100장의 쿠폰이 모두 소진되었습니다")
         }
 
         // 존재하는 회원인지 확인
         val member = memberRepository.findById(memberId)
-            .orElseThrow { RuntimeException("존재하지 않는 회원입니다") }
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다") }
 
         // 만약 회원이 쿠폰을 이미 가지고 있다면, 기존 쿠폰 반환
-        if (member != null) {
-            member.coupon?.let {
-                return it
-            }
+        if (member?.coupon != null) {
+            return member.coupon!!
         }
 
-        // 아니라면 쿠폰을 발행하고 저장
+        // 아니라면 쿠폰을 발행
         val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
         val newCouponCode = (1..10)
             .map { chars.random() }
             .joinToString("")
 
-        // 새로운 쿠폰을 Coupon 엔티티로 저장
+        // 새로운 쿠폰 저장
         val newCoupon = Coupon(couponCode = newCouponCode)
-
-        // 쿠폰 저장
         couponRepository.save(newCoupon)
 
         // 회원의 쿠폰 정보를 업데이트
-        if (member != null) {
-            member.coupon = newCoupon
-        }
-        if (member != null) {
-            member.has_coupon = true
-        }
-        member?.let { memberRepository.save(it) }
+        member?.coupon = newCoupon
+        member?.has_coupon = true
+        memberRepository.save(member!!)
 
         return newCoupon
     }
