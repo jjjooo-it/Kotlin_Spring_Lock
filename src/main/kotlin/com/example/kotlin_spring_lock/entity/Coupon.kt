391d585c12196861bd819c7ff882b8875ee3fa5c
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-02-10 18:51:51 +0900

    fix(service) : 랜덤 문자 에러 해결

--- src/main/kotlin/com/example/kotlin_spring_lock/entity/Coupon.kt
+++ src/main/kotlin/com/example/kotlin_spring_lock/entity/Coupon.kt
@@ -11,13 +11,5 @@ class Coupon (
     var id: Long? = null, // 쿠폰 식별자
 
     @Column(name = "coupon_code", unique = true, nullable = false)
-    var couponCode: String = generateCouponCode(), // 쿠폰 코드 (랜덤 생성 함수 호출)
-
-) {
-    // 쿠폰 코드 랜덤 생성_ 대소문자, 숫자
-    companion object {
-        fun generateCouponCode(): String {
-            return UUID.randomUUID().toString().replace("-", "").substring(0, 10)
-        }
-    }
-}
+    var couponCode: String = "", // 쿠폰 코드
+)
--- src/main/kotlin/com/example/kotlin_spring_lock/service/CouponService.kt
+++ src/main/kotlin/com/example/kotlin_spring_lock/service/CouponService.kt
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ResponseStatusException
-import java.util.*
 
 @Service
 class CouponService(
@@ -21,13 +20,13 @@ class CouponService(
 
         // 쿠폰 갯수가 100장 이상이라면 404 반환
         val couponCount = couponRepository.count()
-        if(couponCount >= 100){
+        if (couponCount >= 100) {
             throw ResponseStatusException(HttpStatus.NOT_FOUND, "쿠폰이 끝났습니다")
         }
 
         // 존재하는 회원인지 확인
         val member = memberRepository.findById(memberId)
-            .orElseThrow({throw RuntimeException("존재하지 않는 회원입니다")})
+            .orElseThrow { RuntimeException("존재하지 않는 회원입니다") }
 
         // 만약 회원이 쿠폰을 이미 가지고 있다면, 기존 쿠폰 반환
         if (member != null) {
@@ -35,10 +34,20 @@ class CouponService(
                 return it
             }
         }
+
         // 아니라면 쿠폰을 발행하고 저장
-        val newCoupon = Coupon(couponCode = UUID.randomUUID().toString())
+        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        val newCouponCode = (1..10)
+            .map { chars.random() }
+            .joinToString("")
+
+        // 새로운 쿠폰을 Coupon 엔티티로 저장
+        val newCoupon = Coupon(couponCode = newCouponCode)
+
+        // 쿠폰 저장
         couponRepository.save(newCoupon)
 
+        // 회원의 쿠폰 정보를 업데이트
         if (member != null) {
             member.coupon = newCoupon
         }
