a32c82a37494051e4e044cecab4e6a1c5ba100fd
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-02-11 10:22:57 +0900

    feat: 기능 구현

---
+++ loadTest.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const memberId = 1;
+export default function () {
+    const res = http.post(`http://localhost:8080/api/coupon/issue`,
+        JSON.stringify({ memberId: memberId }), {
+            headers: { 'Content-Type': 'application/json' },
+        });git
+    sleep(1);
+}
